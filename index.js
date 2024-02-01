fab3b307d31d0f1e42482e6b3edc3adbbdd5ce11
artem <hajnkof@gmail.com>
2024-02-01 14:05:08 +0300

    t1

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const anchors = document.querySelectorAll('a[href*="#"]')
-for (let anchor of anchors) {
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault()
-        const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
-        document.querySelector(goto).scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
-    });
-}
+import scrollTo from "./main";
+
+scrollTo();
---
+++ main.js
@@ -0,0 +1,16 @@
+export default function scrollTo() {
+    const anchors = document.querySelectorAll('a[href*="#"]')
+    for (let anchor of anchors) {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault()
+        const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+        document.querySelector(goto).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+}
+
+}
+
+
